66cce3cc744bc33e381fc0446048fab02582bc59
bilel.belguith <belguithbilel99@gamil.com>
2024-03-24 00:36:07 +0100

    dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,15 @@ import {StudentAddComponent} from "./student-add/student-add.component";
 import {StudentListComponent} from "./student-list/student-list.component";
 import {NgModule} from "@angular/core";
 import {AboutPageComponent} from "./about-page/about-page.component";
+import {EmploiComponent} from "./emploi/emploi.component";
+import {DashboardComponent} from "./dashboard/dashboard.component";
 
-export const routes: Routes = [{path: 'list', component: StudentListComponent},
+export const routes: Routes = [
+  {path: 'list', component: StudentListComponent},
   {path: 'add',   pathMatch: 'full', component: StudentAddComponent},
-  {path: '', redirectTo: '/list', pathMatch: 'full'},  {
-    path: "about",
-    pathMatch: 'full',
-    component: AboutPageComponent
-  },];
+  {path: '', redirectTo: '/list', pathMatch: 'full'},  {path: "about", pathMatch: 'full', component: AboutPageComponent }
+  ,{path: 'emploi', component:EmploiComponent ,  pathMatch: 'full'},{path: 'Dashboard', component:DashboardComponent   ,  pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
